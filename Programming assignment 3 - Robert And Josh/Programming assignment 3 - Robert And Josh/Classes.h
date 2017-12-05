393761af9cabda76f980507872c2b2e8c7c5d5b6
Robert Andersen <robert.andersen@uoit.net>
2017-12-05 16:34:45 -0500

    idk

--- Programming assignment 3 - Robert And Josh/Programming assignment 3 - Robert And Josh/Classes.h
+++ Programming assignment 3 - Robert And Josh/Programming assignment 3 - Robert And Josh/Classes.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 using std::string;
+using namespace std;
 
 class Player
 {
@@ -12,19 +14,19 @@ public:
 };
 class Fighter : public Player
 {
-
+	int attack = 20, damage, armor = 26, health; //stats
 };
 class Wizard : public Player
 {
-
+	int attack, damage = 10 + ((rand % 10) + 5), armor = 18, health; //stats
 };
 class Rogue : public Player
 {
-
+	int attack, damage = 5 + ((rand% 5) + 3), armor = 20, health; //stats
 };
 class Ranger : public Player
 {
-
+	int attack, damage = (14 + ((rand% 10) + 1)) * 2, armor = 21, health; //stats
 };
 Player *player1 = new Player; //creates player1 object
 Player *player2 = new Player; //creates player2 object
